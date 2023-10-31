d1188dc139d30273e4c5c6f0fd3c9f85e8d8e94e
Stefano Balietti <futur.dorko@gmail.com>
2023-10-31 10:10:15 +0100

    sw

--- 7_graph-tool/5_gt_smallworldliness.py
+++ 7_graph-tool/5_gt_smallworldliness.py
@@ -10,35 +10,64 @@ import time
 import os
 
 
-# N = Number of nodes in the graph
-# R = Number of random edges
+def gen_sw(N, R):
+    ## Create a supposedly small world network.
+    g = gt.circular_graph(N)
+    gt.add_random_edges(g, R)
+    return g
 
-def generate_rnd(N, R):
+def test_sw(g):
 
+    g_n_vertices = g.num_vertices()
+    g_n_edges = g.num_edges()
+    g_clustering = gt.global_clustering(g, sampled = True)
+    g_apl = get_avg_apl(g)
 
-    g = gt.circular_graph(100)
-    gt.add_random_edges(g, 30)
+    print()
+    print('Generated test graph with:')
+    print('N nodes:', g_n_vertices)
+    print('N edges:', g_n_edges)
+    print('CC:     ', g_clustering)
+    print('APL:    ', g_apl)
     
+    ## Generated analogous random graph.
 
     g_iter = g.vertices()
 
+    ## We need to keep the same degree distr.
     g_rnd = gt.random_graph(N, lambda: next(g_iter).out_degree(), directed=False)
 
     g_rnd_n_vertices = g_rnd.num_vertices()
     g_rnd_n_edges = g_rnd.num_edges()
     g_rnd_clustering = gt.global_clustering(g_rnd, sampled = True)
+    g_rnd_apl = get_avg_apl(g_rnd)
 
+    print()
     print('Generated random graph with:')
-
-    print('N nodes', g_rnd_n_vertices)
-    print('N edges', g_rnd_n_edges)
-    print('CC:    ', g_rnd_clustering)
-
-    all_sp = gt.shortest_distance(g_rnd)
-    vertex_avgs = gt.vertex_average(g_rnd, all_sp)
-    g_rnd_apl = np.mean(vertex_avgs[0])
-
+    print('N nodes:', g_rnd_n_vertices)
+    print('N edges:', g_rnd_n_edges)
+    print('CC:     ', g_rnd_clustering)
     print('APL:    ', g_rnd_apl)
+
+    SW = compute_sigma(g_clustering, g_apl, g_rnd_clustering, g_rnd_apl)
+    print()
+    print('SW sigma:', SW)
     
-    
-generate_rnd(100, 30)
+def get_avg_apl(gg):
+    all_sp = gt.shortest_distance(gg)
+    vertex_avgs = gt.vertex_average(gg, all_sp)
+    gg_apl = np.mean(vertex_avgs[0])
+    return gg_apl
+
+def compute_sigma(test_cc, test_apl, rnd_cc, rnd_apl):
+    return (test_cc/rnd_cc) / (test_apl/rnd_apl)
+
+
+# N = Number of nodes in the graph
+# R = Number of random edges
+N = 100
+R = 30
+
+g = gen_sw(N, R)
+
+test_sw(g)
